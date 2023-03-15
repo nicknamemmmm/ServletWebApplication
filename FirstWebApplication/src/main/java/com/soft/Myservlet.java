84bb80c84fc63605903f1034a47371163edefcd4
yogesh <chavhanyogesh494@gmail.com>
2023-03-15 20:47:33 +0530

    first servlet Application

---
+++ FirstWebApplication/src/main/java/com/soft/Myservlet.java
@@ -0,0 +1,23 @@
+package com.soft;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Myservlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		
+		
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html");
+		
+		pw.println("<h1>This is my servlet</h1>");
+	}
+
+}
